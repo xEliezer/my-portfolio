e9a40c41a19cca0c280844d566f61b3aca6d6d7f
xEliezer <layugeliezerbongco16@gmail.com>
2023-09-29 02:03:24 +0800

    added footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="">
-            
+        <footer className="container mx-auto my-auto p-8 m-8 ">
+            <hr />
+            <p className="font-semibold text-xl p-8 pb-0">Eliezer Layug</p>
+            <p className="font-semibold text-xl ps-8">2023</p>
         </footer>
     )
 }
